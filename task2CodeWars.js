f626d0a80b17f72a4a7eec1bf8fbd4b0c8471c5c
olyacherednichenko <36190688+olyacherednichenko@users.noreply.github.com>
2020-11-18 17:44:33 +0200

    Update task2CodeWars.js

--- task2CodeWars.js
+++ task2CodeWars.js
@@ -1,5 +1,14 @@
-function stringToArray(string){
+//1.
+  function stringToArray(string){
   const mystring = string.split(" ");
   return  mystring;
 }
+
+//2.
+function DNAtoRNA(dna) {
+  const mydna = dna.replace (/T/g, 'U');
+  return mydna;
+ }
+
+//3.
 
